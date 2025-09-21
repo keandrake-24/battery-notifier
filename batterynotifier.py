0951aa2576f9af07ccdb8b1ebb33852a0e0eb386
keandrake-24 <keandre.imposter@gmail.com>
2025-09-21 10:40:50 +0700

    added force spam notification if battery below 10

--- batterynotifier.py
+++ batterynotifier.py
@@ -11,16 +11,17 @@ while True:
         info[1] = int(info[1][0:-2]) #convert the string percentage to integer to use more than less than operators
     except ValueError:
         continue #sometimes acpi returns the percentage as just charging, so if this happens we just continue
-    if info[0] == 'Discharging,' and info[1] < 120 and notyetcharging != True:
+    if info[0] == 'Discharging,' and info[1] < 25 and notyetcharging != True:
         notyetcharging = True
         sp.run(["sudo","brightnessctl","s","5%"])
         sp.Popen(["paplay","lowbat.mp3"])
-        notification.notify(
-            title='Low Battery',
-            message='Your battery is running low, please plug in your laptop now!',
-            app_name='python-battery',
-            timeout=10  # seconds
-        )
+        sp.Popen(["notify-send","-a","python-battery","Battery low","Your battery is very low, please plug in now!"])
+        
+    elif info[0] == "Discharging," and info[1] < 10 and notyetcharging == True:
+        sp.run(["sudo","brightnessctl","s","5%"])
+        sp.Popen(["paplay","lowbat.mp3"])
+        sp.Popen(["notify-send","-a","python-battery","Battery low","Your battery is very low, please plug in now!"])
+
     elif info[0] == "Charging," and notyetcharging == True:
         notyetcharging = False
         sp.run(["sudo","brightnessctl","s","20%"])
